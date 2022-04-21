0ebd67f0bf1f2662ea70058ddbfe692a69e3b9ba
FayazAntapur <fayazantapur@gmail.com>
2022-04-21 17:02:05 +0530

    added testcase steps

--- src/test/java/verifyNetwork.java
+++ src/test/java/verifyNetwork.java
@@ -1,9 +1,22 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.Connection;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v85.network.Network;
+import org.openqa.selenium.devtools.v85.network.model.ConnectionType;
 import org.testng.annotations.Test;
+
+import java.util.Optional;
 
 public class verifyNetwork {
 
     @Test
     public void OfflineTest(){
         //Need to test offline mode
+
+        ChromeDriver driver = new ChromeDriver();
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+        devTools.send(Network.emulateNetworkConditions(true, 345 ,43545,
+                34545, Optional.of(ConnectionType.BLUETOOTH)));
     }
 }
